64514f2a74bff68def9ae500604a79867b57c192
Jose A. Pinell <japinell@yahoo.com>
2025-09-08 00:11:24 -0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Chat",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 63, 17, 177),
+        ),
+      ),
+      home: Text("TBD"),
+    );
+  }
+}
