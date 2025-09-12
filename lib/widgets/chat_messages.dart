4fe4deb231f34f072489438d32c6b9b17307c173
Jose A. Pinell <japinell@yahoo.com>
2025-09-12 16:18:25 -0700

    Added ChatMessages widget

---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,9 @@
+import "package:flutter/material.dart";
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("This is the chat messages widget."));
+}
