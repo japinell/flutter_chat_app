a1bcb5588f0d66a9dcfe07084ce8a838dd03a34b
Jose A. Pinell <japinell@yahoo.com>
2025-09-12 14:27:11 -0700

    Added _pickImage feature to CustomImagePicker widget

--- lib/widgets/custom_image_picker.dart
+++ lib/widgets/custom_image_picker.dart
@@ -1,4 +1,6 @@
+import "dart:io";
 import "package:flutter/material.dart";
+import "package:image_picker/image_picker.dart";
 
 class CustomImagePicker extends StatefulWidget {
   const CustomImagePicker({super.key});
@@ -10,11 +12,35 @@ class CustomImagePicker extends StatefulWidget {
 }
 
 class _CustomImagePickerState extends State<CustomImagePicker> {
+  File? _pickedImageFile;
+
+  void _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _pickedImageFile = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        CircleAvatar(radius: 40, backgroundColor: Colors.grey),
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage: _pickedImageFile != null
+              ? FileImage(_pickedImageFile!)
+              : null,
+        ),
         TextButton.icon(
           onPressed: () {},
           icon: const Icon(Icons.image),
